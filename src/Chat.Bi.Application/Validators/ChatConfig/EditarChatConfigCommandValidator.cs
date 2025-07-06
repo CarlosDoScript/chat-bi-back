6f5d493edc4039d68985efec6ad399b0a2159ec0
Carlos Henrique <carlos.cicoti@leanwork.com.br>
2025-07-06 00:37:12 -0300

    #Adicionado validação no command de edição do chat config com fluent validation

---
+++ src/Chat.Bi.Application/Validators/ChatConfig/EditarChatConfigCommandValidator.cs
@@ -0,0 +1,25 @@
+﻿using Chat.Bi.Application.Commands.ChatConfig.EditarChatConfig;
+
+namespace Chat.Bi.Application.Validators.ChatConfig;
+
+public class EditarChatConfigCommandValidator : AbstractValidator<EditarChatConfigCommand>
+{
+    public EditarChatConfigCommandValidator()
+    {
+        RuleFor(x => x.CorPrincipal)
+          .NotEmpty()
+          .WithMessage("Cor principal é obrigatório.");
+
+        RuleFor(x => x.CorSecundaria)
+            .NotEmpty()
+            .WithMessage("Cor secundaria é obrigatório.");
+
+        RuleFor(x => x.SaudacaoInicial)
+            .NotEmpty()
+            .WithMessage("Saudação inicial é obrigatório.");
+
+        RuleFor(x => x.Canal)
+            .NotEmpty()
+            .WithMessage("Canal é obrigatório.");
+    }
+}
