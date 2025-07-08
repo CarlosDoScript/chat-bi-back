4ffecff1b1be1d587bb3dc29f6b18d38d95f7e00
Carlos Henrique <carlosmvsep3@hotmail.com>
2025-07-08 12:02:10 -0300

    #Adicionado tipos de base de dados aceitos

---
+++ src/Chat.Bi.Core/Constantes/BaseDeDados/TiposBaseDeDados.cs
@@ -0,0 +1,8 @@
+﻿namespace Chat.Bi.Core.Constantes.BaseDeDados;
+
+public static class TiposBaseDeDados
+{
+    public const string SqlServer = "SqlServer";
+    public const string MySql = "MySql"; 
+    public const string Postgre = "Postgre"; 
+}
